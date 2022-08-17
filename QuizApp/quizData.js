d6b1bafc4e5ca6b9f7d441064a675d3d317ea81a
farhad <ferry.norooz@gmail.com>
2022-08-17 00:13:05 -0700

    first commit

---
+++ QuizApp/quizData.js
@@ -0,0 +1,30 @@
+const quizData = [
+    {
+        question:'How old is Florin?',
+        a:'10',
+        b:'18',
+        c:'21',
+        d:'110',
+        correct:'c'
+    },{
+        question:'Who is the president of US ?',
+        a:'Donald Trump',
+        b:'Farhad Noruzzade',
+        c:'Ali Molamahmudi',
+        d:'Mehrdad Ayazi',
+        correct:'a',
+    },{
+        question:'What HTML stands for ?',
+        a:'Cascading style sheets',
+        b:'Hypertext markup language',
+        c:'Cristiano Ronaldo',
+        d:'Game of Throns',
+        correct:'b',
+    },{
+        question:'Which programming language is most usable in 2019 ?',
+        a:'Java',
+        b:'Javascript',
+        c:'C',
+        d:'C#',
+        correct:'a'
+    }]
